9596d7584fa55d10a6b9091c00514079c3b795ae
CoderEssaiydy <bilalessaiydy2021@gmail.com>
2025-02-26 15:25:57 +0000

    Add files via upload

--- projet 5.py
+++ projet 5.py
@@ -1,11 +1,10 @@
-colors = []
-colors.append (input ("add the first color you like :\n"))
-choice = input ("do you want to add more colors: yes or no : \n").lower()
-if choice == "yes" :
-    colors.append (input ("add another color like are :\n"))
-    print("the colors you like are :")
-    print (colors)
-else : 
-    print ("the color you like is :")
-    print (colors)
-
+print("welcome to place the cat : 🐈")                                                        
+rond =[ [ "🐁" , "🐁" , "🐁" ] , [ "🐁" , "🐁" , "🐁" ] , [ "🐁" , "🐁" , "🐁"  ] ]
+print(f"{rond[0]} \n{rond[1]} \n{rond[2]}")
+print("where should the rabbit go 🐈")
+chose = input ("please choose a row and a colum :\n")
+row = int(chose[0])-1
+colum = int(chose [1])-1 
+rond[row][colum] = "🐈"
+print ("you succes ....")
+print (f"{rond[0]} \n{rond[1]} \n{rond[2]}")
